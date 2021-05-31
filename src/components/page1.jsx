5411bc349f2438d1dc9a0569712beca0d7e2ead6
chtholly <271268970@qq.com>
2021-05-31 16:48:07 +0800

    新增react

--- src/components/page1.jsx
+++ src/components/page1.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React , { useState, useEffect }from 'react';
+import getdata from '../lib/getdata'
 
 function Page1({}){
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+        getdataAction()
+    },[]);
+
+    function getdataAction(){
+        getdata("api/test.json").then(data => {
+            console.log(data)
+        })
+    }
+    
     return (
         <div>
-            page12
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
         </div>
     )
 }
