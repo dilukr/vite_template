47c5f61ebdf86a962a1143821bd78e6611599cda
chtholly <271268970@qq.com>
2021-06-21 20:41:47 +0800

    增加echarts模块

--- src/components/page3.jsx
+++ src/components/page3.jsx
@@ -1,7 +1,35 @@
 import React , { useState, useEffect }from 'react';
 
+import ReactEcharts from 'echarts-for-react';
+// 引入 ECharts 主模块
+import echarts from 'echarts/lib/echarts';
+// 引入柱状图
+import 'echarts/lib/chart/bar';
+// 引入提示框和标题组件
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/title';
+
+
+function barOption(){
+    let option = {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'bar'
+        }]
+    };
+    return option
+}
+
+
 function Page3() {
-    return <div>hello</div>
+    return <div><ReactEcharts option={barOption()} /></div>
 }
 
 export default Page3;
