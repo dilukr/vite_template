75353fb86dbcbacdecd5ef58e1b21bd0417aaac9
chtholly <271268970@qq.com>
2021-06-21 19:34:52 +0800

    增加测试的antd组件

--- src/components/page2.jsx
+++ src/components/page2.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import getdata from '../lib/getdata';
+import { Modal, Button } from 'antd';
 
 class Page2 extends React.Component {
     state = {
-        count: 0
+        count: 0,
+        visible: false
     }
     componentDidMount (){
         getdata("api/test.json").then(data => {
@@ -14,10 +16,29 @@ class Page2 extends React.Component {
         })
     }
 
+    handleOk =() => {
+        this.setState({
+            visible:false
+        })
+    }
+
+    showModal = () => {
+        this.setState({
+            visible:true
+        })
+    }
+
     render (){
         return (
             <div>
-                {this.state.count}
+                <Button type="primary" onClick={this.showModal}>
+                    Open Modal
+                </Button>
+                <Modal title="Basic Modal" visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleOk}>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
             </div>
         )
     }
