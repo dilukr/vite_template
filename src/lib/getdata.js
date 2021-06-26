324b39757eb3b3075b387cf2d97acedf8f9cb71e
zhongli <271268970@qq.com>
2021-06-26 17:33:27 +0800

    新增函数说明注释

--- src/lib/getdata.js
+++ src/lib/getdata.js
@@ -12,6 +12,22 @@ function parseJSON(response) {
 }
 
 
+/**
+ *
+ * 请求获取页面中的数据
+ * @export
+ * @param {*} url 地址
+ * @returns
+ * 使用方法
+ * import getdata from '[相对路径]'
+ * 
+ * getdata("url")
+ * .then(data => {
+ *
+ * })
+ * 
+ * 
+ */
 export default function getdata(url) {
     return new Promise((resolve, reject) => {
         fetch(url,{credentials: 'include'})
