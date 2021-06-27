c1200a9c436c6eb3641f9b4cd4a279e6cbd53813
chtholly <271268970@qq.com>
2021-06-27 16:07:38 +0800

    新增项目文档说明

--- src/main.jsx
+++ src/main.jsx
@@ -13,9 +13,9 @@ import {
 
 
 //引入分页
-import Page1 from './components/page1';
-import Page2 from './components/page2';
-import Page3 from './components/page3';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
 
 export default function App() {
   return (
